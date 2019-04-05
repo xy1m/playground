6db97c699248b1f3cb4ab7bc789933d75cf3803e
George Zhenpeng <zhangzhpeng@gmail.com>
2019-04-04 22:51:53 -0700

    Add RodCud

--- src/main/java/com/xy1m/playground/algorithms/dp/Q15_1_RodCut.java
+++ src/main/java/com/xy1m/playground/algorithms/dp/Q15_1_RodCut.java
@@ -1,11 +1,11 @@
-package com.xy1m.playground.algorithms;
+package com.xy1m.playground.algorithms.dp;
 
 import java.util.Arrays;
 
 /**
  * Created by gzhenpeng on 2019/4/2
  */
-public class RodCut {
+public class Q15_1_RodCut {
     static int cutRecursive(int[] prices, int n) {
         if (n == 0) return 0;
         int res = Integer.MIN_VALUE;
@@ -45,10 +45,35 @@ public class RodCut {
         return res[n];
     }
 
+    static int cutRodBottomUpExtended(int[] prices, int[] s, int n) {
+        int[] res = new int[prices.length];
+        for (int i = 1; i <= n; i++) {
+            int q = Integer.MIN_VALUE;
+
+            for (int j = 1; j <= i; j++) {
+                if (q < prices[j] + res[i - j]) {
+                    q = prices[j] + res[i - j];
+                    s[i] = j;
+                }
+            }
+            res[i] = q;
+        }
+        return res[n];
+    }
+
     public static void main(String[] args) {
         int[] prices = new int[]{0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-        System.out.println(cutRecursive(prices, 10));
-        System.out.println(cutRodMemoized(prices, 10));
-        System.out.println(cutRodBottomUp(prices, 10));
+        int n = 10;
+        System.out.println(cutRecursive(prices, n));
+        System.out.println(cutRodMemoized(prices, n));
+        System.out.println(cutRodBottomUp(prices, n));
+
+        int[] solutions = new int[11];
+        int rev = cutRodBottomUpExtended(prices, solutions, n);
+        System.out.println(rev);
+        while (n > 0) {
+            System.out.println(solutions[n]);
+            n = n - solutions[n];
+        }
     }
 }
